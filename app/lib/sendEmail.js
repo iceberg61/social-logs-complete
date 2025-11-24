d77fd01c2850b5efa96081b093bbccac931de2d2
Godstime Godwin <godstimegodwin111@gmail.com>
2025-11-24 11:52:19 +0100

    resend test-2

--- app/lib/sendEmail.js
+++ app/lib/sendEmail.js
@@ -1,21 +1,19 @@
-import { Resend } from 'resend';
+import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function sendEmail({ to, subject, html }) {
+export default async function sendEmail(to, subject, html) {
   try {
     const data = await resend.emails.send({
-      from: process.env.EMAIL_FROM,
+      from: process.env.RESEND_FROM_EMAIL,
       to,
       subject,
       html,
     });
-    console.log(' Email sent:', data);
-    return true;
+
+    return { success: true, data };
   } catch (error) {
-    console.error(' Email error:', error);
-    return false;
+    console.error("Email error:", error);
+    return { success: false, error };
   }
 }
-
-export default sendEmail;
