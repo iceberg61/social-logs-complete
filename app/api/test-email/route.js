9b0c6e027d4997c755af4c34c99e321bded907d7
Godstime Godwin <godstimegodwin111@gmail.com>
2025-11-24 12:01:10 +0100

    deleted test-email

--- app/api/test-email/route.js
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import  sendEmail from "@/lib/sendEmail";
-
-export async function GET() {
-  await sendEmail("yourPersonalEmail@example.com", "Test Email", "<p>Working!</p>");
-  return NextResponse.json({ message: "Email sent" });
-}
